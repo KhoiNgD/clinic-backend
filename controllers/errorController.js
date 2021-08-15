033db72b409a58b536f0cd29b10c9394c34d09b0
Khoi Nguyen <khoindct123@gmail.com>
2021-08-15 23:34:15 +0700

    add send production error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -27,6 +27,44 @@ const sendErrorDev = (err, req, res) => {
   });
 };
 
+const sendErrorProd = (err, req, res) => {
+  // A) API
+  if (req.originalUrl.startsWith("/api")) {
+    // A) Operational, trusted error: send message to client
+    if (err.isOperational) {
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+    }
+    // B) Programming or other unknown error: don't leak error details
+    // 1) Log error
+    console.error("ERROR 💥", err);
+    // 2) Send generic message
+    return res.status(500).json({
+      status: "error",
+      message: "Something went very wrong!",
+    });
+  }
+
+  // B) RENDERED WEBSITE
+  // A) Operational, trusted error: send message to client
+  if (err.isOperational) {
+    return res.status(err.statusCode).render("error", {
+      title: "Something went wrong!",
+      msg: err.message,
+    });
+  }
+  // B) Programming or other unknown error: don't leak error details
+  // 1) Log error
+  console.error("ERROR 💥", err);
+  // 2) Send generic message
+  return res.status(err.statusCode).render("error", {
+    title: "Something went wrong!",
+    msg: "Please try again later.",
+  });
+};
+
 module.exports = (err, req, res, next) => {
   if (err instanceof ValidateError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
