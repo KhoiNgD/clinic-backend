5e0cd48267bf9b9a8ae23713cdd2ba54658744ea
Khoi Nguyen <khoindct123@gmail.com>
2021-08-15 23:29:31 +0700

    add error controller

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -42,5 +42,17 @@ module.exports = (err, req, res, next) => {
 
   if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, req, res);
+  } else if (process.env.NODE_ENV === "production") {
+    let error = { ...err };
+    error.message = err.message;
+
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === "ValidationError")
+      error = handleValidationErrorDB(error);
+    if (error.name === "JsonWebTokenError") error = handleJWTError();
+    if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
+
+    sendErrorProd(error, req, res);
   }
 };
