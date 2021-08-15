a0cca28e83e871078356d4700e89d745c12a3039
Khoi Nguyen <khoindct123@gmail.com>
2021-08-15 15:56:22 +0700

    cannot booking when there is pending appointment

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,6 +1,6 @@
 const Booking = require("../models/bookingModel");
 const Clinic = require("../models/clinicModel");
-const User = require("../models/userModel");
+const AppError = require("../errors/AppError");
 const catchAsync = require("../utils/catchAsync");
 
 exports.getAllBookings = catchAsync(async (req, res, next) => {
@@ -16,15 +16,41 @@ exports.getAllBookings = catchAsync(async (req, res, next) => {
 });
 
 exports.createBooking = catchAsync(async (req, res, next) => {
+  const userId = req.user._id;
   const { clinicId } = req.params;
   const { bookedDate, bookedTime } = req.body;
-  console.log(bookedDate);
-  console.log(bookedTime);
+
+  const convertDateToTimeNumber = (date) =>
+    new Date(date).getHours() * 60 + new Date(date).getMinutes();
+
+  // Check if there is pending appointment
+  // between user and clinic
+  const pendingBooking = await Booking.findOne({
+    $and: [{ clinic: clinicId }, { user: userId }, { status: "pending" }],
+  });
+  if (pendingBooking) {
+    const now = new Date(Date.now());
+
+    // Check if user booking time is after pending appointment
+    const isAfterPendingAppointment =
+      now.setHours(0, 0, 0, 0) >= new Date(bookedDate).setHours(0, 0, 0, 0) &&
+      convertDateToTimeNumber(now) > convertDateToTimeNumber(bookedTime);
+
+    if (isAfterPendingAppointment) {
+      pendingBooking.status = "denied";
+      await pendingBooking.save();
+    } else {
+      return res.status(409).json({
+        status: "fail",
+        message: "Unable to book an appointment. You have a pending request.",
+      });
+    }
+  }
+
   const booking = new Booking();
-  booking.bookedDate = bookedDate;
-  booking.bookedTime =
-    new Date(bookedTime).getHours() * 60 + new Date(bookedTime).getMinutes();
-  booking.user = req.user._id;
+  booking.bookedDate = new Date(bookedDate);
+  booking.bookedTime = convertDateToTimeNumber(bookedTime);
+  booking.user = userId;
   booking.clinic = clinicId;
   await booking.save();
 
