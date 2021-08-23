c8e7956adb2386c7dbc73bc55296ef9a95caa7ed
Khoi Nguyen <khoindct123@gmail.com>
2021-08-23 19:19:56 +0700

    fix update user profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,15 +8,24 @@ exports.getUser = factory.getOne(User);
 exports.updateUser = catchAsync(async (req, res, next) => {
   const { id } = req.params;
   const { name, email, phone } = req.body;
+  console.log(req.file);
   const updateValues = { name, email, phone };
   const user = await User.findByIdAndUpdate(id, updateValues, { new: true });
-  user.avatar = {
-    url: req.file?.path ?? "",
-    filename: req.file?.filename ?? "",
-  };
-  await user.save();
+
+  if (req.file) {
+    user.avatar = {
+      url: req.file?.path ?? "",
+      filename: req.file?.filename ?? "",
+    };
+    await user.save();
+  }
 
   if (req.body.deleteAvatar) {
+    user.avatar = {
+      url: "",
+      filename: "",
+    };
+    await user.save();
     await cloudinary.uploader.destroy(req.body.deleteAvatar);
   }
 
